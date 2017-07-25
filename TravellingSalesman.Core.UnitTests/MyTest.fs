e931659872ab3e8da34994133ea83721a2ccfd2a
Cameron Lind <mediocresurgeon@gmail.com>
2017-07-26 11:37:13 +1200

    Got rid of trash.

--- TravellingSalesman.Core.UnitTests/MyTest.fs
+++
@@ -1,8 +0,0 @@
-﻿module TravelingSalesman.Core.UnitTests.MyTest
-
-open NUnit.Framework
-open TravelingSalesman.Core
-
-[<Test>]
-let ``Swap two adjacent elements``() = 
-    Assert.AreEqual(Say.getNumber, 1)
