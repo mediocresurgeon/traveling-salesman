a988c75b979d8fadea904e1377efc9cdc76e9abb
Cameron Lind <mediocresurgeon@gmail.com>
2017-07-27 10:13:43 +1200

    TravelingSalesman.Core.IO now has a way to read sets of named coordinates from a Stream (such as a FileStream).  It also has a way to write named, timestamped coordinates to a Stream (such as a FileStream).  The writing function has not yet been tested and may contain bugs!

---
+++ TravelingSalesman.Core.UnitTests/IOTest.fs
@@ -0,0 +1,35 @@
+﻿module TravelingSalesman.Core.UnitTests.IOTest
+
+open System.Reflection
+open NUnit.Framework
+open TravelingSalesman.Core.DTO
+open TravelingSalesman.Core.IO
+
+// https://stackoverflow.com/questions/1175056/value-of-the-last-element-of-a-list#answer-1175123
+let rec last = function
+    | hd :: [] -> hd
+    | hd :: tl -> last tl
+    | _ -> failwith "Empty list."
+
+[<Test>]
+let ``Deserialize Example.gpx``() = 
+    // Arrange
+    let assembly = Assembly.GetEntryAssembly()
+    use stream = assembly.GetManifestResourceStream("Example.gpx")
+    // Example.gpx is an embedded resource
+
+    // Act
+    let coordinates = ReadLocationsFromGpxStream stream
+
+    // Assert
+    Assert.IsNotEmpty(coordinates)
+
+    let firstCoordinate = coordinates.Head
+    Assert.AreEqual("Hagatna Pillbox", firstCoordinate.Name)
+    Assert.AreEqual(13.47852, firstCoordinate.LatitudeDegrees)
+    Assert.AreEqual(144.7516, firstCoordinate.LongitudeDegrees)
+
+    let finalCoordinate = last coordinates
+    Assert.AreEqual("Chief Quipuha Statue", finalCoordinate.Name)
+    Assert.AreEqual(13.47725, finalCoordinate.LatitudeDegrees)
+    Assert.AreEqual(144.7538, finalCoordinate.LongitudeDegrees)
---
+++ TravelingSalesman.Core/IO.fs
@@ -0,0 +1,81 @@
+﻿namespace TravelingSalesman.Core
+
+open System
+open System.ComponentModel
+open System.IO
+open System.Runtime.Serialization
+open System.Xml.Serialization
+open TravelingSalesman.Core.DTO
+
+module IO = 
+
+// Some of these items are marked with [<EditorBrowsable(EditorBrowsableState.Never)>]
+// This prevents them from showing up in Intellisense.
+// These items should have been marked Private or Internal,
+// but had to be marked Public in order for the XmlSerializer to do work on them.
+// Hiding them from intellisense should make it clear that these items are not intended for public use.
+
+    [<CLIMutable>]
+    [<DataContract>]
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    type public WaypointInput = {
+        [<XmlAttribute("lat")>]
+        Latitude : float<degree>
+        [<XmlAttribute("lon")>]
+        Longitude : float<degree>
+        [<XmlElement("name")>]
+        Name : string
+    }
+
+    [<DataContract>]
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    type public WaypointOutput = {
+        [<XmlAttribute("lat")>]
+        Latitude : float<degree>
+        [<XmlAttribute("lon")>]
+        Longitude : float<degree>
+        [<XmlElement("name")>]
+        Name : string
+        [<XmlElement("time")>]
+        Timestamp: DateTime
+    }
+
+    [<CLIMutable>]
+    [<DataContract>]
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    [<XmlRoot(ElementName = "gpx")>]
+    type public GpxInput = {
+        [<XmlElement("wpt")>] 
+        Waypoints : WaypointInput array
+    }
+
+    [<DataContract>]
+    [<EditorBrowsable(EditorBrowsableState.Never)>]
+    [<XmlRoot(ElementName = "gpx")>]
+    type public GpxOutput = {
+        [<XmlElement("wpt")>]
+        Waypoints: WaypointOutput array
+    }
+
+    let private ConvertWaypointInputToLocation (waypoint:WaypointInput) =
+        new Location(waypoint.Name, waypoint.Latitude, waypoint.Longitude)
+    
+    let private ConvertWaypointToWaypointOutput (waypoint:Waypoint) = 
+        {
+            Latitude = waypoint.LatitudeDegrees;
+            Longitude = waypoint.LongitudeDegrees;
+            Name = waypoint.Name;
+            Timestamp = waypoint.Timestamp
+        }
+    
+    let public ReadLocationsFromGpxStream (gpxFile:Stream) =
+        let xmlSerializer = new XmlSerializer(typedefof<GpxInput>)
+        let dataContractResult = xmlSerializer.Deserialize(gpxFile) :?> GpxInput // http://stackoverflow.com/questions/31616761/f-casting-operators
+        dataContractResult.Waypoints
+            |> Array.toList
+            |> List.map (fun wpt -> ConvertWaypointInputToLocation wpt)
+
+    let public WriteWaypointsToGpxStream (waypoints:List<Waypoint>, gpxFile:Stream) = 
+        let xmlSerializer = new XmlSerializer(typedefof<GpxOutput>)
+        let gpx = { Waypoints = waypoints |> List.map (fun wpt -> ConvertWaypointToWaypointOutput wpt) |> List.toArray }
+        xmlSerializer.Serialize(gpxFile, gpx)
