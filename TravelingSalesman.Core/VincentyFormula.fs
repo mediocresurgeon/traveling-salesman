697fb1f1d25d577ed5b286c9303d57573bd67792
Cameron Lind <mediocresurgeon@gmail.com>
2017-08-20 13:49:40 +1200

    Simplified Vincenty Formula.

--- TravelingSalesman.Core/VincentyFormula.fs
+++ TravelingSalesman.Core/VincentyFormula.fs
@@ -8,6 +8,21 @@ open TravelingSalesman.Core.DTO
 /// Discovered by Thadeus Vincenty in 1975.
 /// </summary>
 module VincentyFormula = 
+    [<NoEquality;NoComparison>]
+    type private IntermediateData =
+        {
+            sinλ : float;
+            cosλ : float;
+            sinSqσ : float;
+            sinσ : float;
+            cosσ : float;
+            σ : float;
+            sinα : float;
+            cosSqα : float;
+            cos2σM : float;
+            C : float;
+        }
+
     let private reciprocal (n:float) = 
         1.0 / n
         
@@ -24,6 +39,8 @@ module VincentyFormula =
 
     let private toSin (tanθ:float) (cosθ:float) = 
         tanθ * cosθ
+       
+
 
     let public GetDistance (semiMajorAxis:float<'u>, semiMinorAxis:float<'u>) (location1:IRadialCoordinate) (location2:IRadialCoordinate) =
         // F# translation of "Direct and Inverse Solutions of Geodesics
@@ -45,60 +62,45 @@ module VincentyFormula =
         let cosU2 = toCos tanU2
         let sinU2 = toSin tanU2 cosU2
 
-        let eq1 (sinλ:float) (cosλ:float) =
-            (cosU2 * sinλ) * (cosU2 * sinλ) + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) * (cosU1 * sinU2 - sinU1 * cosU2 * cosλ)
-
-        let eq2 (cosλ:float) = 
-            sinU1 * sinU2 + cosU1 * cosU2 * cosλ
-
-        let eq3 (sinλ:float) (sinσ:float) = 
-            cosU1 * cosU2 * sinλ / sinσ
-
-        let eq4 (sinα:float) = 
-            1.0 - (sinα ** 2.0)
-
-        let eq5 (cosσ:float) (cosSqα:float) = 
-            cosσ - 2.0 * sinU1 * sinU2 / cosSqα
-
-        let eq6 (cosSqα:float) = 
-            f / 16.0 * cosSqα * (4.0 + f * (4.0 - 3.0 * cosSqα))
-
         let mutable λ = L
         let mutable λʹ = λ
-        let mutable sinλ = sin λ
-        let mutable cosλ = cos λ
-        let mutable sinSqσ = eq1 sinλ cosλ
-        let mutable sinσ = sqrt sinSqσ
-        let mutable cosσ = eq2 cosλ
-        let mutable σ = atan2 sinσ cosσ
-        let mutable sinα = eq3 sinλ sinσ
-        let mutable cosSqα = eq4 sinα
-        let mutable cos2σM = eq5 cosσ cosSqα
-        let mutable C =  eq6 cosSqα
+        let mutable loopCount = 0
 
-        let looper = 
-            sinλ <- sin λ
-            cosλ <- cos λ
-            sinSqσ <- eq1 sinλ cosλ
-            sinσ <- sqrt sinSqσ
-            cosσ <- eq2 cosλ
-            σ <- atan2 sinσ cosσ
-            sinα <- eq3 sinλ sinσ
-            cosSqα <- eq4 sinα
-            cos2σM <- eq5 cosσ cosSqα
-            C <-  eq6 cosSqα
+        let rec getIntermediateResults() = 
+            loopCount <- loopCount + 1
+            let sinλ = sin λ
+            let cosλ = cos λ
+            let sinSqσ = (cosU2 * sinλ) * (cosU2 * sinλ) + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) * (cosU1 * sinU2 - sinU1 * cosU2 * cosλ)
+            let sinσ = sqrt sinSqσ
+            let cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+            let σ = atan2 sinσ cosσ
+            let sinα = cosU1 * cosU2 * sinλ / sinσ
+            let cosSqα = 1.0 - (sinα ** 2.0)
+            let cos2σM = cosσ - 2.0 * sinU1 * sinU2 / cosSqα
+            let C = f / 16.0 * cosSqα * (4.0 + f * (4.0 - 3.0 * cosSqα))
             λʹ <- λ
             λ <- L + (1.0-C) * f * sinα * (σ + C*sinσ*(cos2σM+C*cosσ*(-1.0+2.0*cos2σM**2.0)))
+            if (λ-λʹ |> abs > 0.000000000001 && loopCount < 200) then
+                getIntermediateResults()
+            else
+                {
+                    sinλ = sinλ
+                    cosλ = cosλ
+                    sinSqσ = sinSqσ
+                    sinσ = sinσ
+                    cosσ = cosσ
+                    σ = σ
+                    sinα = sinα
+                    cosSqα = cosSqα
+                    cos2σM = cos2σM
+                    C = C
+                }
 
-        looper
-        let mutable loopCount = 0
-        while (λ-λʹ |> abs > 0.000000000001 && loopCount < 200) do
-            looper
-            loopCount <- loopCount + 1
-        
-        let uSq = cosSqα * (a*a - b*b) / (b*b)
-        let A = 1.0 + uSq/16384.0*(4096.0+uSq*(-768.0+uSq*(320.0-175.0*uSq)))
-        let B = uSq/1024.0 * (256.0+uSq*(-128.0+uSq*(74.0-47.0*uSq)))
-        let Δσ = B*sinσ*(cos2σM+B/4.0*(cosσ*(-1.0+2.0*cos2σM*cos2σM)-B/6.0*cos2σM*(-3.0+4.0*sinσ*sinσ)*(-3.0+4.0*cos2σM*cos2σM)))
+        let calcs = getIntermediateResults()
+                
+        let uSq = calcs.cosSqα * (a*a - b*b) / (b*b)
+        let A = 1.0 + uSq / 16384.0 * (4096.0 + uSq * (-768.0 + uSq * (320.0 - 175.0 * uSq)))
+        let B = uSq / 1024.0 * (256.0 + uSq * (-128.0 + uSq * (74.0 - 47.0 * uSq)))
+        let Δσ = B * calcs.sinσ * (calcs.cos2σM + B / 4.0 * (calcs.cosσ * (-1.0 + 2.0 * calcs.cos2σM * calcs.cos2σM) - B / 6.0 * calcs.cos2σM * (-3.0 + 4.0 * calcs.sinσ * calcs.sinσ) * (-3.0 + 4.0 * calcs.cos2σM * calcs.cos2σM)))
 
-        b*A*(σ-Δσ)
+        b*A*(calcs.σ-Δσ)
