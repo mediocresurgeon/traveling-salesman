6d567180898e0f15dc55af45df5b12eb3a9de6dd
Cameron Lind <mediocresurgeon@gmail.com>
2017-09-03 16:42:11 +1200

    Fixed a few bugs.  Created simple SA algorithm.  Wrote some program code to help try to determine what good temperature and cooling rate values are.

--- TravelingSalesman.Application/Program.fs
+++ TravelingSalesman.Application/Program.fs
@@ -1,7 +1,182 @@
 ﻿// Learn more about F# at http://fsharp.org
+
 open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Threading
+open TravelingSalesman.Core.Common
+open TravelingSalesman.Core.TspAlgorithms.SimulatedAnnealing
+
+
+/// <summary>
+/// Runs a task, but cancels it if it takes too long.
+/// </summary>
+/// <param name="timeout">In milliseconds.</param>
+/// <param name="action">The task to execute.</param>
+/// <returns>The result of the task.</returns>
+let withTimeout timeout action = 
+    // https://stackoverflow.com/a/32744262
+    async {
+        let! child = Async.StartChild(action, timeout)
+        return! child
+    }
+
+
+/// <summary>
+/// Tries to find a travelling salesman solution.
+/// Will time out.
+/// </summary>
+/// <param name="timeout">In milliseconds.</param>
+/// <param name="random">A random number generator (RNG).</param>
+/// <param name="startingTemp">The starting temperature.</param>
+/// <param name="finalTemp">The desired final temperature.</param>
+/// <param name="coolingRate">The cooling rate.</param>
+/// <param name="knownDistances">A dictionary of known distances.</param>
+/// <param name="locations">The collection of locations to optimize.</param>
+/// <returns>A solution to the travelling salesman problem.</returns>
+let getSolutionWithTimeout timeout random startingTemp finalTemp coolingRate knownDistances locations =
+    async {
+        let finder =
+            async {
+                let getSolution = OptimizePathHaversine (startingTemp, finalTemp, coolingRate) random
+                let bestRoute = getSolution locations
+                let bestDistance = List.toArray bestRoute
+                                   |> GetRelativeDistance knownDistances
+                return bestRoute
+            }
+        return! withTimeout timeout finder    
+    }
+
+
+
+
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World from F#!"
+    let locations = [
+        new Location("The Atrium - Massey University", -36.7331<degree>, 174.7011<degree>);
+        new Location("Aperture Memorial", -36.7328<degree>, 174.7006<degree>);
+        new Location("Massey IMS Building", -36.7338<degree>, 174.7012<degree>);
+        new Location("Sir Neil Water Lecture Hall", -36.7338<degree>, 174.7019<degree>);
+        new Location("Massey University Quadrangle Building", -36.7324<degree>, 174.7017<degree>);
+        new Location("Matauranga Stained Window", -36.7324<degree>, 174.7013<degree>);
+        new Location("Massey University Lost Sign", -36.7319<degree>, 174.7014<degree>);
+        new Location("Study Centre Opening Plaque", -36.7316<degree>, 174.7004<degree>);
+        new Location("Massey University Study Centre Courtyard", -36.7311<degree>, 174.7006<degree>);
+        new Location("Albany Presbyterian Church", -36.730<degree>, 174.6977<degree>);
+        new Location("Albany Village Cemetery", -36.7298<degree>, 174.6972<degree>);
+        new Location("Albany Community Playground", -36.7286<degree>, 174.6982<degree>);
+        new Location("Albany Community House", -36.7284<degree>, 174.6988<degree>);
+        new Location("King George V Coronation Hall", -36.727<degree>, 174.6971<degree>);
+        new Location("Albany Memorial Library", -36.7264<degree>, 174.6968<degree>);
+        new Location("Holy Cross Anglican Church", -36.7253<degree>, 174.6961<degree>);
+        new Location("Tranquil Space", -36.7253<degree>, 174.695<degree>);
+        new Location("\"Balance\" Bronze Sculpture", -36.7254<degree>, 174.6949<degree>);
+        new Location("Kell Park Tree House", -36.72525<degree>, 174.6941<degree>);
+        new Location("Kell Park Heritage Trail", -36.725<degree>, 174.6942<degree>);
+        new Location("Albany Village Community Anchor", -36.7246<degree>, 174.6944<degree>);
+        new Location("Lucas Landing", -36.72345<degree>, 174.6929<degree>);
+        new Location("Kell Park Phillips Property", -36.7242<degree>, 174.693<degree>);
+        new Location("Art in the Park", -36.7242<degree>, 174.6933<degree>);
+        new Location("Albany Orchards", -36.7248<degree>, 174.6934<degree>);
+        new Location("The Oldtimer - Daniel Lucas", -36.7254<degree>, 174.6933<degree>);
+        new Location("Kell Park Bridge Entrance", -36.7259<degree>, 174.6936<degree>);
+        new Location("North Harbour Stadium Sculpture", -36.7269<degree>, 174.7028<degree>);
+        new Location("Albany Stadium", -36.7278<degree>, 174.7023<degree>);
+        new Location("Don Munri Plaque", -36.7261<degree>, 174.7026<degree>);
+        new Location("Harbour Sports Sports House", -36.7261<degree>, 174.703<degree>);
+        new Location("Marist North Harbour Rugby Club", -36.7259<degree>, 174.7035<degree>);
+        new Location("North Harbour RC Club Race Track", -36.7258<degree>, 174.7043<degree>);    
+        new Location("North Harbour Stadium Archway", -36.7248<degree>, 174.705<degree>);
+        new Location("Iron Beanstalk", -36.7229<degree>, 174.7052<degree>);   
+        new Location("Bronze Stars", -36.7228<degree>, 174.7047<degree>);
+        new Location("Hooton Reserve", -36.7227<degree>, 174.7039<degree>);   
+        new Location("Hooton Reserve Park Map", -36.722<degree>, 174.7052<degree>);
+        new Location("Hooton Reserve Skate Bowl", -36.7212<degree>, 174.7053<degree>);
+        new Location("Hooton Reserve Playground", -36.7211<degree>, 174.7056<degree>);
+        new Location("Kawai Purapura", -36.7211<degree>, 174.7044<degree>);
+    ]
+    let random = new Random()
+
+    let knownDistances = new Dictionary<(Location * Location), float>()
+
+    (*
+    // Correct magnitudes!
+    let startingTemp = 0.000001
+    let finalTemp = 0.0000000000000001
+    let coolingRate = 0.01
+    *)
+
+    let startingTemp = 0.0000005
+    let finalTemp = 0.0000000000000001
+    let coolingRate = 0.01
+
+    //let timeOutInMilliseconds = 1000
+    //let initialStartingTemp   = 10000000000.0
+    //let initialFinalTemp      = 10000000000.0
+    //let coolingRate           = 0.001
+
+    //let mutable bestResult = 1000.0
+    //let mutable bestCombination = initialStartingTemp, initialFinalTemp, coolingRate
+    //for startingMult in 0..20 do
+    //    let startingTemp = 10.0 ** -(float startingMult)
+    //    for finalMult in 0..20 do
+    //        let finalTemp = 10.0 ** -(float finalMult)
+    //        try
+    //            let calc =
+    //                getSolutionWithTimeout timeOutInMilliseconds random startingTemp finalTemp coolingRate
+    //            let mutable solutions:List<float> = new List<float>()
+    //            for attempts in 0..100 do
+    //                calc knownDistances locations
+    //                |> Async.RunSynchronously
+    //                |> List.toArray
+    //                |> GetRelativeDistance knownDistances
+    //                |> solutions.Add
+    //                |> ignore
+    //            let average = solutions.ToArray()
+    //                          |> Array.toList
+    //                          |> List.sum
+    //                          |> (fun total -> total / float solutions.Count)
+    //            if average < bestResult then
+    //                bestResult <- average
+    //                bestCombination <- startingTemp, finalTemp, coolingRate
+    //                printfn "New best!\nstartingTemp: %e\nfinalTemp: %e\ncoolingRate: %e" startingTemp finalTemp coolingRate
+    //        with
+    //        | _ -> ()
+    
+    //let s, f, c = bestCombination
+    //printfn "startingTemp: %e\n finalTemp: %e\n coolingRate: %e" s f c 
+    //printfn "average: %g" bestResult
+    let originalDistance = List.toArray locations
+                           |> GetRelativeDistance knownDistances
+    
+    printfn "Original distance: %A" originalDistance
+
+    let mutable newRoute = List.toArray locations
+                           |> ShuffleArray random
+                           |> Array.toList
+                           |> OptimizePathHaversine (startingTemp, finalTemp, coolingRate) random
+    let mutable newDistance = List.toArray newRoute
+                              |> GetRelativeDistance knownDistances
+    let mutable attempts = 1
+
+    while originalDistance <= newDistance do
+        attempts <- attempts + 1
+        let candidateRoute = OptimizePathHaversine (startingTemp, finalTemp, coolingRate) random newRoute
+        let candidateDistance = List.toArray candidateRoute
+                                |> GetRelativeDistance knownDistances
+        printfn "Attempt result:    %A" candidateDistance
+        if candidateDistance < newDistance then
+            newRoute    <- candidateRoute
+            newDistance <- candidateDistance
+
+    for coordinate in newRoute do
+        printfn "%s: %f, %f" coordinate.Name coordinate.LatitudeDegrees coordinate.LongitudeDegrees
+
+    //if newDistance <= originalDistance then
+        //for coordinate in newRoute do
+        //printfn "%s: %f, %f" coordinate.Name coordinate.LatitudeDegrees coordinate.LongitudeDegrees
+    
+    printfn "Final distance:    %A" newDistance
+    printfn "Attempts:          %i" attempts
     0 // return an integer exit code
--- TravelingSalesman.Core/SimulatedAnnealing.fs
+++ TravelingSalesman.Core/SimulatedAnnealing.fs
@@ -17,7 +17,7 @@ module SimulatedAnnealing =
     /// <param name="index">The known index.</param>
     /// <param name="arrayLength">The index count of the array.</param>
     /// <returns>The adjacent index.</returns>
-    let internal getAdjacentIndex (index:int) (arrayLength:int) = 
+    let internal GetAdjacentIndex (index:int) (arrayLength:int) = 
         (index + 1) % arrayLength
 
             
@@ -28,11 +28,12 @@ module SimulatedAnnealing =
     /// <param name="index1">The index of the first element to swap.</param>
     /// <param name="index2">The index of the second element to swap.</param>
     /// <returns>A new array.</returns>
-    let internal swapElementsAtIndex (array:array<'T>) (index1:int) (index2:int) = 
+    let internal SwapElementsAtIndex (array:array<'T>) (index1:int) (index2:int) = 
         let arrayCopy = Array.copy array
-        let element1 = arrayCopy.[index1]
-        arrayCopy.[index1] <- arrayCopy.[index2]
-        arrayCopy.[index2] <- element1
+        let element1 = Array.get arrayCopy index1
+        let element2 = Array.get arrayCopy index2
+        Array.set arrayCopy index1 element2
+        Array.set arrayCopy index2 element1
         arrayCopy
 
 
@@ -42,14 +43,14 @@ module SimulatedAnnealing =
     /// <param name="random">The random number generator to use.</param>
     /// <param name="array">The array to read from.</param>
     /// <returns>A new array.</returns>
-    let internal shuffleArray (random:Random) (array:array<'T>) = 
+    let public ShuffleArray (random:Random) (array:array<'T>) = 
         // Fisher–Yates shuffle
         // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
         let mutable newArray = array
-        for i in (array.Length-1)..1 do
-            let randomIndex = random.Next(0, i+1) // first argument is inclusive; second argument is exclusive
+        for i in 0..(array.Length-2) do
+            let randomIndex = random.Next(i, array.Length) // first argument is inclusive; second argument is exclusive
             if i <> randomIndex then
-                newArray <- swapElementsAtIndex newArray i randomIndex
+                newArray <- SwapElementsAtIndex newArray i randomIndex
         newArray
 
 
@@ -59,10 +60,10 @@ module SimulatedAnnealing =
     /// <param name="random">The random number generator to use.</param>
     /// <param name="array">The array to read from.</param>
     /// <returns>A new array.</returns>
-    let internal swapAdjacentRandom (random:Random) (array:array<'T>) = 
+    let internal SwapAdjacentRandom (random:Random) (array:array<'T>) = 
         let randomIndex = random.Next(array.Length)
-        let adjIndex = getAdjacentIndex randomIndex array.Length
-        swapElementsAtIndex array randomIndex adjIndex
+        let adjIndex = GetAdjacentIndex randomIndex array.Length
+        SwapElementsAtIndex array randomIndex adjIndex
 
 
     /// <summary>
@@ -70,14 +71,16 @@ module SimulatedAnnealing =
     /// </summary>
     /// <param name="location">The Location object to convert.</param>
     /// <returns>A tuple of (Latitude, Longitude) in radians.</returns>
-    let internal toTuple (location:IRadialCoordinate) =
+    let internal ToTuple (location:IRadialCoordinate) =
         location.LatitudeRadians, location.LongitudeRadians
 
 
     /// <summary>
     /// Determines the angle between two points on the surface of a sphere.
     /// </summary>
-    /// <remarks>Looking up a known result in a dictionary is faster than performing the same calculation over and over again.</remarks>
+    /// <remarks>
+    /// Looking up a known result in a dictionary is faster than performing the same calculation over and over again.
+    /// </remarks>
     /// <param name="previousResults">A dictionary of the results of previous calculations.  This eliminates computational redundancy.</param>
     /// <param name="location1">The first location to compare.</param>
     /// <param name="location2">The second location to compare.</param>
@@ -88,15 +91,96 @@ module SimulatedAnnealing =
         // 2a) If a result was already calculated, return that result
         // 2b) Otherwise, calculate the result, save it to the dictionary and return the result
         
-        let sortedLocations = [| location1; location2; |] |> Seq.sortBy(fun l -> l.Name)
+        let sortedLocations = [| location1; location2; |]
+                              |> Seq.sortBy(fun l -> l.Name)
         let firstLoc = Seq.head sortedLocations
-        let secondLoc = Seq.tail sortedLocations |> Seq.head
+        let secondLoc = Seq.tail sortedLocations
+                        |> Seq.head
         let dicKey = (firstLoc, secondLoc)
 
         match previousResults.TryGetValue dicKey with
-                         | true, value -> value
-                         | _           -> let p1 = toTuple location1
-                                          let p2 = toTuple location2
-                                          let angle = HaversineFormula.GetAngle p1 p2
-                                          previousResults.Add(dicKey, angle)
-                                          angle
+        | true, value -> value
+        | _           -> let p1 = ToTuple location1
+                         let p2 = ToTuple location2
+                         let angle = HaversineFormula.GetAngle p1 p2
+                         previousResults.Add(dicKey, angle)
+                         angle
+
+    
+    /// <summary>
+    /// Determines the relative distance of a collection of locations on the surface of a perfect sphere.
+    /// </summary>
+    let public GetRelativeDistance (knownDistances:Dictionary<(Location * Location), float>) (locations:array<Location>) =
+        let mutable runningTotal = 0.0
+        let arrayLength = locations.Length
+        for index in 0..(arrayLength-1) do
+            let adjacentIndex = GetAdjacentIndex index arrayLength
+            let location1 = Array.get locations index
+            let location2 = Array.get locations adjacentIndex
+            let angle = GetAngle knownDistances location1 location2
+            runningTotal <- runningTotal + angle
+        runningTotal
+             
+
+    /// <summary>
+    /// Determines the probability that a given solution will be accepted.
+    /// </summary>
+    /// <remarks>
+    /// This keeps the Simulated Annealing algorithm from getting stuck in a local minima.
+    /// </remarks>
+    let internal GetAcceptanceProbability (currentEnergy:float) (newEnergy:float) (temperature:float) =
+        if (currentEnergy > newEnergy) then
+            1.0
+        else
+            Math.Exp((currentEnergy - newEnergy) / temperature)
+
+
+    /// <summary>
+    /// Calculates an efficient circuit through all locations.
+    /// Distances are compared using the Haversine Formula.
+    /// </summary>
+    let public OptimizePathHaversine (startingTemperature:float, finalTemperature:float, coolingRate:float) (random:Random) (locations:list<Location>) = 
+        // 1) Begin with a random collection of locations.
+        // 2) Loop until computation budget is spent:
+        // 2.1) Swap two adjacent elements
+        // 2.2) Record the new result
+        // 2.3) Compare the new distance to the previous best distance.
+        // 2.3.1) If the new distance is better, keep the new best result and loop again.
+        // 2.3.2) If the new distance is worse...
+        // 2.3.2.1) Determine the acceptance probability
+        // 2.3.2.2) Randomly determine whether the new value is accepted as a best result
+        // 2.3.2.2.1) If the new value is accepted, replace keep the best result and loop again.
+
+        let distances = new Dictionary<(Location * Location), float>()
+
+        let mutable bestSolution = List.toArray locations
+                                   //|> (ShuffleArray random)
+        let arrayLength = Array.length bestSolution
+
+        /// <summary>
+        /// Determines the weight of a given distance solution.
+        /// </summary>
+        let getSolutionEnergy (distance:float) = 
+            distance / float arrayLength
+
+        let mutable bestSolutionEnergy = GetRelativeDistance distances bestSolution
+                                         |> getSolutionEnergy
+        
+        let budgetMultiplier = 1.0 - coolingRate
+        let mutable currentResult = Array.copy bestSolution
+        let mutable computationBudget = startingTemperature
+        while computationBudget > finalTemperature do
+            currentResult <- SwapAdjacentRandom random currentResult
+            let currentSolutionEnergy = GetRelativeDistance distances currentResult
+                                        |> getSolutionEnergy
+            if currentSolutionEnergy < bestSolutionEnergy then
+                bestSolution <- currentResult
+                bestSolutionEnergy <- currentSolutionEnergy
+            else
+                let acceptanceprobability = GetAcceptanceProbability bestSolutionEnergy currentSolutionEnergy computationBudget
+                if acceptanceprobability > random.NextDouble() then
+                    bestSolution <- currentResult
+                    bestSolutionEnergy <- currentSolutionEnergy
+            computationBudget <- computationBudget * budgetMultiplier
+
+        Array.toList bestSolution
